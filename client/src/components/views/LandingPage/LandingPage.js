32ae25a62e06a210976f99d36c0d2ee4829d42e0
ysyoo <ysyoo@simplexi.com>
2021-09-08 15:37:57 +0900

    logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -7,11 +7,20 @@ function LandingPage() {
     .then(response => console.log(response.data))
   }, [])
 
+  //function
+  const onClickHandler = () => {
+    axios.get('/api/users/logout')
+    .then(response=> {
+      console.log(response.data)
+    })
+  }
 
   return (
     <div style={{display:'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
     }}>
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   )
 }
